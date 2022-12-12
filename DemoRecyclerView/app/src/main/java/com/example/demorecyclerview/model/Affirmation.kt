8c1e55f0cd0c1281416ca9d18cbd99cbc0da1b3f
peem <61626732+peemmiz@users.noreply.github.com>
2022-12-12 16:14:03 +0700

    Add files via upload

---
+++ DemoRecyclerView/app/src/main/java/com/example/demorecyclerview/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.example.demorecyclerview.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+    )
